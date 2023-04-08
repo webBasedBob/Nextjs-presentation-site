d68aff4a403e067b94ae70aade3979cf66e780b5
webBasedBob <catalin.b.pop97@gmail.com>
2023-04-09 00:31:07 +0300

    pp

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
-export default function Home() {
-  return <h1>Home page</h1>;
+import fs from "fs/promises";
+import path from "path";
+export default function Home(props) {
+  return <h1>{props.header}</h1>;
+}
+
+export async function getStaticProps() {
+  let datapath = path.join(process.cwd(), "data", "dummy-data.json");
+  let jsonData = await fs.readFile(datapath);
+  let data = JSON.parse(jsonData);
+  return {
+    props: data.homePage[0],
+  };
 }
